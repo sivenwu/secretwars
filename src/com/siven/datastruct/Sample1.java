7324f0cb7e49dbffb9f5ddda84d822cbfe2857ef
wusiyuan <wusiyuan@lizhi.fm>
2021-04-10 12:47:44 +0800

    feat: sample提交

---
+++ src/com/siven/datastruct/Sample1.java
@@ -0,0 +1,16 @@
+package com.siven.datastruct;
+
+import com.siven.datastruct.tree.model.TreeNode;
+
+/**
+ * author siven
+ * date: 2020/7/26
+ * desc: 一句话概括
+ **/
+public class Sample1 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/com/siven/datastruct/Sample2.java
@@ -0,0 +1,14 @@
+package com.siven.datastruct;
+
+/**
+ * author siven
+ * date: 2020/7/26
+ * desc: 一句话概括
+ **/
+public class Sample2 {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
